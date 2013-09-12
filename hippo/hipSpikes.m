c4aa4ba9a6c237b0b361cb25cf8bd41a7949915b
Gautam Agarwal <infinitesimally@hotmail.com>
2013-09-11 19:19:39 -0700

    09-11-13lab

--- hippo/hipSpikes.m
+++ hippo/hipSpikes.m
@@ -3,16 +3,16 @@ function [spikeMat cellInfo] = hipSpikes(file,bin,d,subSet)%spk %a b c badUnits
 %bin usually 32/1.25
 %%new way for Kenji's data
 
-load('/media/Expansion Drive/redwood/KenjiData.mat');
-% %load('/media/work/hippocampus/KenjiData.mat');
+%load('/media/Expansion Drive/redwood/KenjiData.mat');
+load('/media/work/hippocampus/KenjiData.mat');
 whichDay = strcmp(file,Beh(:,4));
 dayID = Beh(whichDay,2);
-dayCells = PyrIntMap.Map(:,1) == find(strcmp(dayID,PyrIntMap.fileBase));% & Region  == 1;%> 3;%
+dayCells = PyrIntMap.Map(:,1) == find(strcmp(dayID,PyrIntMap.fileBase)) & Region  == 1;%> 3;%
 %d = ['/media/Kenji_data/' Beh{whichDay,3} '/' Beh{whichDay,1} '/' file '/'];
 someShanks = unique(PyrIntMap.Map(dayCells,3));
 %%old way
 if ~exist('d','var')
-    d = ['/media/Expansion Drive/KenjiMizuseki/'];%['/media/work/hippocampus/' file '/'];%
+    d = ['/media/work/hippocampus/' file '/'];%['/media/Expansion Drive/KenjiMizuseki/'];%
 end
 if exist('someShanks','var') && ~isempty(someShanks)
     [a,b,c,d] = LoadCluRes([d file],someShanks);
