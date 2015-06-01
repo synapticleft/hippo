87e2b05e48384a4c2ff7425ff608b8fd15180b4a
synapticleft <infinitesimally@hotmail.com>
2015-06-01 11:30:44 +0100

    ""

--- lisbon/myTrajICA.m
+++ lisbon/myTrajICA.m
@@ -1,15 +1,15 @@
-function [wx Xor trialNum stimOr] = myTrajICA(fn,ILD)
+function [wx Xor trialNum stimOr X] = myTrajICA(fn,ILD)
 
 inds = [15 16];
 if ~exist('ILD','var')
     ILD = [.5 2 4];
 end
-cols = [linspace(0,1,64);zeros(1,64);linspace(1,0,64)]';
+%[linspace(0,1,64);linspace(0,1,64);linspace(1,0,64)]';
 %cols = colormap('jet');
 %d = preProcessRoberto(fn,[inds 6],0,0,[],[-ILD ILD]);
 
 file = dir('*.mat');
-load(file(fn).name,'data');
+load(file(fn).name,'data','center_ILD');
 choice = [data{2:end,7}];
 f = choice ~= 3;
 
@@ -25,21 +25,22 @@ for i = 2:size(data,1)%1:size(d,1)
     %trialData = trialData(:,~isnan(data{i,6}));
     %traj = squeeze(d(i,:,1) + 1i*d(i,:,2));%
     if version == 1
-        trajcwt = [cwt(real(diff(traj,[],2)),scales,'cgau1'); cwt(imag(diff(traj,[],2)),scales,'cgau2')];
+        trajcwt = [cwt(real(diff(traj,[],2)),scales,'cgau1'); cwt(imag(diff(traj,[],2)),scales,'cgau1')];
         trajcwt = [zeros(size(trajcwt,1),1) trajcwt];
     elseif version == 2
-            trajcwt = [cwt(real(traj),scales,'cgau1'); cwt(imag(traj),scales,'cgau2')];
+            trajcwt = [cwt(real(traj),scales,'cgau1'); cwt(imag(traj),scales,'cgau1')];
     else
             trajcwt = cwt(traj,scales,'cgau1');
     end
     trajcwt = trajcwt(:,~isnan(data{i,6}));
     %trajcwt = [cwt(squeeze(d(i,:,1)),scales,'cgau1');cwt(squeeze(d(i,:,2)),scales,'cgau1')];
     %trajcwt = log(max(eps,abs(cwt(squeeze(d(i,:,1)),scales,'cgau1')))) + 1i*log(max(eps,abs(cwt(squeeze(d(i,:,2)),scales,'cgau1'))));
-    stimcwt = cwt(data{i,6}(~isnan(data{i,6})),scales,'cgau1');
+    stimDat = data{i,6} - center_ILD;
+    stimcwt = cwt(stimDat(~isnan(data{i,6})),scales,'cgau1');
     X = [X trajcwt];%(:,f)];
     Xor = [Xor traj(:,~isnan(data{i,6}))];%(f)];
     stim = [stim stimcwt];
-    stimOr = [stimOr data{i,6}(~isnan(data{i,6}))];
+    stimOr = [stimOr stimDat(~isnan(data{i,6}))];
     %dx = squeeze(d(i,f,1));
     %dy = squeeze(d(i,f,2));
     %dx = filtfilt(gausswin(8),sum(gausswin(8)),dx);
@@ -58,22 +59,32 @@ isReal = 0;
 if isReal
 [wx A W ] = fastica([real(X);imag(X)],'approach','symm');
 else
-[A W Z] = cfastica(X);%,'mle_circ');%
+[A W] = cfastica(X);%,'mle_circ');% Z
+%[U,S,V] = svd(X,'econ');
+%A = U*S;W = pinv(A);
 wx = W*X;
-ang = angle(mean(sign(real(wx)).*wx,2)).';
-%ang = angle(mean(A(1:end/2,:)));
-A = bsxfun(@times,A,exp(1i*ang));W = bsxfun(@times,W,exp(1i*-ang).');wx = bsxfun(@times,wx,exp(1i*-ang).');
+%wx = bsxfun(@rdivide,wx,std(wx,0,2));
+for i = 1:size(wx,1)
+    [temp,~] = svds([real(wx(i,:));imag(wx(i,:))],1);
+    ang(i) = exp(1i*atan(temp(2)/temp(1)));%angle(mean(sign(real(wx)).*wx,2)).';
 end
+temp = [];
+%ang = angle(mean(A(1:end/2,:)));
+A = bsxfun(@times,A,ang);W = bsxfun(@times,W,conj(ang).');wx = bsxfun(@times,wx,conj(ang).');
+end
+figure;scatter(real(A(:)),imag(A(:)),'filled');drawnow;
 [~,s] = sort(sum(A.*conj(A)),'descend');
 A = A(:,s); W = W(s,:);wx = wx(s,:);
 figure;imagesc(complexIm(A));
 %figure;imagesc(complexIm(A));
+trialNum = trialNum(trialNum ~= 0);
 trialNum = [0 trialNum];
-return
+%return
 figure;
 xs{1} = -3:.1:3;xs{2} = xs{1};for i = 1:min(16,size(wx,1))
 subplot(4,4,i);imagesc(log(hist3([real(wx(i,:)); imag(wx(i,:))]',xs)));
 end
+%wx = real(wx);
 %return
 figure;for i = 1:min(25,size(wx,1))
 subplot(5,5,i);
@@ -94,17 +105,23 @@ end
 imagesc(complexIm(temp));
 end
 %return
-figure;for i = 1:numel(trialNum)-1
-    inds = trialNum(i)+1:trialNum(i+1);
+figure;
+cols = colormap('jet');
+%for i = 1:numel(trialNum)-1
+    inds = trialNum(30):trialNum(50);%trialNum(i)+1:trialNum(i+1);
     for j = 1:min(25,size(wx,1))
         %acc1 = abs(W(j,:)*X(:,inds));acc1 = [max(1,min(ceil(acc1/max(acc1)*64),64))];
         act = wx(j,inds);
-        cols = squeeze(complexIm(act,0,.5));indPlot = abs(act) > 1;
-        subplot(5,5,j);scatter(real(Xor(inds(indPlot))),imag(Xor(inds(indPlot))),abs(act(indPlot))*10,cols(indPlot,:),'filled');hold all;axis tight
+         indPlot = abs(act) > 1;
+         act = act(indPlot);%act = wx(j,inds(indPlot));
+        actI = max(1,min(64,32+round(10*real(act))));
+        %cols = squeeze(complexIm(act,0,.5));
+        subplot(5,5,j);scatter(real(Xor(inds(indPlot))),imag(Xor(inds(indPlot))),abs(act)*10,cols(actI,:),'filled');%hold all;
+        axis tight
+        drawnow;
     end
-    drawnow;
     %input('');
-end
+%end
 %     acc = X([ord end],inds); %- X([ord end]-1,inds);%*2 + X([ord end] -2,inds);
 %     fs = scal2frq(2.^(0:.1:5),'cgau1',1/80);
 %     tempx = cwt(acc(1,:),2.^(0:.1:5),'cgau1');
