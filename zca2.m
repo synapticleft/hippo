7e37fbca7d91b1b6ecfa0dcf7682cf59bdc457cc
synapticleft <infinitesimally@hotmail.com>
2015-07-17 10:42:40 +0100

    ""

--- zca2.m
+++ zca2.m
@@ -1,16 +1,20 @@
 function [Z,V,E,D] = zca2(X)
-eps = 1e-4;%10
+epsilon = 1e-10;
 % Calculate the eigenvalues and eigenvectors of the new covariance matrix.
 covarianceMatrix = X*X'/size(X,2);
-[E, D] = eig(covarianceMatrix);
+% [E, D] = eig(covarianceMatrix);
+% 
+% % Sort the eigenvalues  and recompute matrices
+% [~,order] = sort(diag(-D));
+% E = E(:,order);
+% d = diag(D); 
+% dsqrtinv = real((d + eps).^(-0.5)); %ones(size(d))*
+% Dsqrtinv = diag(dsqrtinv(order));
+% D = diag(d(order));
+% V = E*Dsqrtinv*E';
+% Z = V*X;
 
-% Sort the eigenvalues  and recompute matrices
-[~,order] = sort(diag(-D));
-E = E(:,order);
-d = diag(D); 
-dsqrtinv = real((d + ones(size(d))*eps).^(-0.5));
-Dsqrtinv = diag(dsqrtinv(order));
-D = diag(d(order));
-V = E*Dsqrtinv*E';
-Z = V*X;
 
+[U,S] = svd(covarianceMatrix);
+V = U * diag(1./sqrt(diag(S) + epsilon)) * U';
+Z = V * X;
