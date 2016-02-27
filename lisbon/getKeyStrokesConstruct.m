a5faa49c0bc9807e366b0691d22a292c04f75d0e
synapticleft <infinitesimally@hotmail.com>
2016-02-27 17:35:56 +0000

    ""

--- lisbon/getKeyStrokesConstruct.m
+++ lisbon/getKeyStrokesConstruct.m
@@ -1,37 +1,34 @@
 function fullData = getKeyStrokesConstruct %[dataOut,pos]
-%reads all of the files in a directory and organizes the data into a struct
-%according to the identity of the player
-% 'Pos' stores all vertices visited and their times
-% 'Strokes' stores all keystrokes
-% 'Lose' stores all losses
-% 'Pop' stores all bubble loss events
-% 'Date' stores identifying date (first play logged by browser)
+%% reads all of the files in a directory and organizes the data into a struct
+%% with a separate field for each player (ASSUMES EACH PLAYER ENTERS A DIFFERENT NAME
 % 'Name' stores player name
-% 'CPU' stores device type
-% 'gametype' stores number of vertices
+% 'System' stores device type (eg. windows, linux, osx)
+% 'session' stores info for each game played by player, with fields:
+%   'Time' stores when game was played
+%   'numVertices' stores number of vertices
+%   'event' stores all events in a game, each with 9 entries:
+%       Time
+%       event type (key presses and other game events) *See below for how each event is represented*
+%       wave number
+%       current score
+%       amount in bank
+%       angle of spinner
+%       size of polygon corresponding to score
+%       size of polygon corresponding to bank
+%       time within the current wave
 
-%Time
-%event
-%wave
-%score
-%bank
-%angle
-%graphicScore
-%graphicBank
-%waveTime
-%L = lose, V = vertex, B = bubble
-num = [-40,-39,-38,-37,0,1,37,38,39,40,.1,.2,.3];
+% press (left, up, right, down); release (left up right down);land on vertex; lose life; lose bubble   
+num = [-40,-39,-38,-37,37,38,39,40,.1,.2,.3]%;0,1,
 files = dir('*.txt');
-%cols = ['b','r'];
 fullData.users = {};
 for k = 1:numel(files)
-    data = loadjson(files(k).name);%file1);
+    data = loadjson(files(k).name);
     data = data.data;
     eq = [];
     for l = 1:numel(fullData.users)
-        eq(l) = streq(data{1}{1},fullData.users{l}.name) && streq(data{1}{3},fullData.users{l}.system);
+        eq(l) = strcmp(data{1}{1},fullData.users{l}.name) && strcmp(data{1}{3},fullData.users{l}.system);
     end
-    if ~sum(eq)%~ismember(data{1,1}{1},names)
+    if ~sum(eq)
         if isempty(l)
             l = 1;
         else
@@ -46,71 +43,20 @@ for k = 1:numel(files)
     fullData.users{l}.session{end+1}.time = data{1}{2};
     fullData.users{l}.session{end}.numVertices = data{1}{4};
     fullData.users{l}.session{end}.event = [];
-%     fullData.users{l}.session{end}.keyPress = [];
-%     fullData.users{l}.session{end}.keyRelease = [];
-%     fullData.users{l}.session{end}.position = [];
-%     fullData.users{l}.session{end}.lose = [];
-%     fullData.users{l}.session{end}.pop = [];
-    %startFile1 = data{1}{2}/86400/1000 + datenum(1970,1,1);
-    
     for i = 3:length(data)-1
         if ~iscell(data{i})
-%             if data{i}(2) > 0
-%                 fullData.users{l}.session{end}.keyPress(end+1,:) = data{i}';
-%             else
-%                 fullData.users{l}.session{end}.keyRelease(end+1,:) = data{i}';
-%             end
-            %        dataOut(i-2,:) = data{i};
             fullData.users{l}.session{end}.event(end+1,:) = data{i};
         else
             if data{i}{2}{1} == 'V'
-                fullData.users{l}.session{end}.event(end+1,:) = [cell2mat(data{i}(1)) .1 cell2mat(data{i}(3:end))];%position
+                fullData.users{l}.session{end}.event(end+1,:) = [cell2mat(data{i}(1)) .1 cell2mat(data{i}(3:end))];%vertex land
             elseif data{i}{2}{1} == 'L'
-                fullData.users{l}.session{end}.event(end+1,:) = [cell2mat(data{i}(1)) .2  cell2mat(data{i}(3:end))];%lose
+                fullData.users{l}.session{end}.event(end+1,:) = [cell2mat(data{i}(1)) .2  cell2mat(data{i}(3:end))];%lose game
             elseif data{i}{2}{1} == 'B'
-                fullData.users{l}.session{end}.event(end+1,:) = [cell2mat(data{i}(1)) .3 cell2mat(data{i}(3:end))];%pop
+                fullData.users{l}.session{end}.event(end+1,:) = [cell2mat(data{i}(1)) .3 cell2mat(data{i}(3:end))];%lose bubble
             end
-            %         for j = 1:9
-            %             if iscell(data{i}{j})
-            %                 if data{i}{j}{1} == 'V'%data{i}{j}{1}(1) == 'R'
-            %                     dataOut(i-2,j) = -1;%dataOut(i-2,j) = -1*str2num(data{i}{j}{1}(2:end));
-            %                 elseif data{i}{j}{1} == 'L'
-            %                     dataOut(i-2,j) = 0;
-            %                 elseif data{i}{j}{1} == 'B'
-            %                     dataOut(i-2,j) = 1;
-            %                 else
-            %                     dataOut(i-2,j) = str2num(data{i}{j}{1});
-            %                 end
-            %             else
-            %                 dataOut(i-2,j) = data{i}{j};
-            %             end
-            %         end
         end
     end
     fullData.users{l}.session{end}.event(:,6) = mod(fullData.users{l}.session{end}.event(:,6),360);
     fullData.users{l}.session{end}.event(~ismember(fullData.users{l}.session{end}.event(:,2),num),:) = [];
     fullData.users{l}.session{end}.event(fullData.users{l}.session{end}.event(:,9) == 0,:) = [];
-%    fullData.users{l}.session{end}.keyRelease(~ismember(fullData.users{l}.session{end}.keyRelease(:,2),num),:) = [];
-%dataOut(dataOut(:,9) == 0,:) = [];
-%dataOut(dataOut(:,6) == 360,6) = 0;
-%pos = dataOut(dataOut(:,2) == -1,:);
-%dataOut(dataOut(:,2) == -1,:) = [];
-%dataOut(~ismember(dataOut(:,2),num),:) = [];
-%dataOut(dataOut(:,3) == 0,3) = 8;
-
-% for l = 1:numel(names)
-%     eq(l) = streq(data{1,1}{1},names{l});
-% end
-% figure(1);hold all;
-% brk = find(abs(diff(pos(:,6))) > 100 | diff(pos(:,9) < 0));
-% brk = [0 brk' size(pos,1)];
-% posa = pos(:,9)  + (pos(:,3)-1)*40;%pos(:,1);
-% for m = 1:numel(brk)-1
-%     plot(posa((brk(m)+1):brk(m+1)),pos((brk(m)+1):brk(m+1),6),cols(find(eq)));
-% end
-% temp = dataOut(:,9)+(dataOut(:,3)-1)*40;%dataOut(:,1);
-% drawnow;
-% figure(2);hold all;plot(temp,dataOut(:,4)+dataOut(:,5),cols(find(eq)));drawnow;
-% %figure(49);hold all;plot(dataOut(:,9));
-% %figure(50);hold all;plot(dataOut(:,9)+40*(dataOut(:,3)-1));
 end
--- lisbon/spinnerOptimumP.m
+++ lisbon/spinnerOptimumP.m
@@ -5,6 +5,7 @@ function [traj,act, score, rawValues,VValues] = spinnerOptimumP(fname,wavenums)
 %also differs from spinnerOptimum4 because it looks only n seconds into the
 %future.
 
+temperature = .001;%temperature for soft-max action selection
 dat = importdata(fname);
 dat = dat(ismember(dat(:,3), wavenums),:);
 for i = 1:numel(wavenums)
@@ -25,10 +26,9 @@ travelcost = .0; %cost of moving per vertex, 0 appears to be OK!!
 movecost = .01; %cost of moving at all -- prevents agent from taking a break while moving to a far target
 popcost = movecost; %cost of pop action -- discourages gratuitous pressing
 spawnAccess = [poppabletime 1];
+rampVal = linspace(spawnAccess(1),spawnAccess(2),diff(spawnAccess)*growtime/dt+1);
 
 rawValues = zeros(numStates,totallen/dt);
-rampVal = linspace(spawnAccess(1),spawnAccess(2),diff(spawnAccess)*growtime/dt+1);
-%rampTime = linspace(spawnAccess(1)*growtime,spawnAccess(2)*growtime,diff(spawnAccess)*growtime/dt+1);
 theBubble = rawValues;
 
 for i = 1:size(dat,1)
@@ -36,11 +36,10 @@ for i = 1:size(dat,1)
     rawValues(dat(i,2)+1,inds) = rampVal;
     theBubble(dat(i,2)+1,inds) = i;
 end
-VValues = zeros(numStates,size(rawValues,2)+1);
-QValues = zeros(numStates,numStates*2,size(rawValues,2)+1);
-isPopped = cell(size(VValues));
-act = zeros(size(rawValues));
-
+VValues = zeros(numStates,size(rawValues,2));
+QValues = zeros(numStates,numStates*2,size(rawValues,2));
+%isPopped = cell(size(VValues));
+%act = zeros(size(rawValues));
 
 uncertainT = .1; %std / sec;
 thresh = .001;
@@ -54,44 +53,58 @@ for i = 1:size(timeFilt,2)
     timeFilt(:,i) = exp(-(xs.^2)/(uncertainT*i*dt).^2/2);
     timeFilt(:,i) = timeFilt(:,i)/sum(timeFilt(:,i));
 end
-for i = 1:(totallen-wavegap)/dt
+%stimChunk = zeros(numStates,growtime/dt+numel(xs)/2);
+rawChunk = zeros(1,growtime/dt);
+Vlocal = zeros(numStates,size(stimChunk,2)+1);
+Qlocal = zeros(numStates,numStates*2,size(stimChunk,2)+1);
+for i = 1:size(rawValues,2)
     %take chunk of stimuli, blurred with temporal uncertainty
-    stimPresent = rawValues(:,i) > 0;
-    stimChunk = rawValues(:,i+(1:(growtime/dt+numel(xs)/2))-1);
-    for j = 1:size(rawValues,1)
-        if ~stimPresent(j)
-            stimChunk(j,:) = 0;
-        else
-            ind = find(stimChunk(j,:) == 0,1);
-            stimChunk(j,ind:end) = 0;
-            temp = toeplitz(zeros(1,numel(xs)),stimChunk(j,:));
-            stimChunk(j,1:growtime/dt) = sum(temp(:,numel(xs)/2+1:end).*timeFilt);
-        end
+%    stimChunk(:) = 0;
+    rawChunk(:) = 0;
+    visBubbles = find(dat(:,1) > i*dt - growtime & dat(:,1) <= i*dt);
+    stimChunk = zeros(numel(visBubbles),growtime/dt+numel(xs)/2);
+    for k = 1:numel(visBubbles)
+        inds = max((dat(visBubbles(k),1) + growtime*poppabletime)/dt-i+1,1):((dat(visBubbles(k),1)+growtime)/dt - i+1);  
+        rawChunk(:) = 0;
+        rawChunk(inds) = rampVal(end-numel(inds):end);
+        temp = toeplitz(zeros(1,numel(xs)),rawChunk);
+        stimChunk(k,:) = sum(temp(:,numel(xs)/2+1:end).*timeFilt);
+%         rawChunk(:) = 0;
+%         rawChunk(inds) = 1;
+%         temp = toeplitz(zeros(1,numel(xs)),rawChunk);
+%         stimChunk(k,:) = sum(temp(:,numel(xs)/2+1:end).*timeFilt);
     end
-    Vlocal = zeros(numStates,growtime/dt+1);
-    for ii = growtime/dt:-1:1
-        for j = 1:numStates %%THIS IS WHERE I AM
+    Vlocal(:) = 0;
+    Qlocal(:) = 0;
+    VBlocal = zeros(numStates,numel(visBubbles),size(Vlocal,2));
+    VBtemp = zeros(numStates*2,numel(visBubbles));
+    for ii = size(stimChunk,2):-1:1
+        for j = 1:numStates
+            curBubble = find(dat(visBubbles,2)+1 == j);
             for k = 1:numStates
                 indsX(k) = mod(j-k-1,numStates)+1;
                 indsY(k) = round(ii+1+slidertime/dt*min(k,numStates-k));
                 indsY(k+numStates) = round(indsY(k)+popTime/dt);
                 indsY([k k+numStates]) = min(size(Vlocal,2),indsY([k k+numStates]));
+                VBtemp(k,:) = VBlocal(indsX(k),:,indsY(k)); %Needed??
+                VBtemp(k+numStates,:) = VBtemp(k,:);
                 valNext(k) = Vlocal(indsX(k),indsY(k))-travelcost*min(k,numStates-k) - movecost*(k ~= numStates);
-                valNext(k+numStates) = Vlocal(indsX(k),indsY(k+numStates))-travelcost*min(k,numStates-k) ...
-                    - movecost*(k ~= numStates)-popcost + stimChunk(j,ii);
-                if ~ismember(theBubble(j,ii),isPopped{indsX(k),indsY(k+numStates)})
-                    valNext(k+numStates) = valNext(k+numStates) - ;
+                if ~numel(curBubble)
+                    valNext(k+numStates) = -inf;
+                else
+                    VBtemp(k+numStates,curBubble) = stimChunk(curBubble,1);
+                    valNext(k+numStates) = Vlocal(indsX(k),indsY(k+numStates))-travelcost*min(k,numStates-k) ...
+                        - movecost*(k ~= numStates)-popcost + stimChunk(curBubble,ii) - VBlocal(indsX(k),curBubble,indsY(k+numStates));
                 end
             end
-            QValues(j,:,i) = valNext;
-            [VValues(j,i),act(j,i)] = max(valNext);
-            if act(j,i) > numStates
-                isPopped{j,i} = union(theBubble(j,i),isPopped{indsX(act(j,i)-numStates),indsY(act(j,i))});
-            else
-                isPopped{j,i} = isPopped{indsX(act(j,i)),indsY(act(j,i))};
-            end
+            Qlocal(j,:,ii) = valNext;
+            pAct = exp(valNext/temperature)/sum(exp(valNext/temperature));
+            Vlocal(j,ii) = pAct*valNext;
+            VBlocal(j,:,ii) = bsxfun(@times,VBtemp,pAct);
         end
     end
+    VValues(:,i) = Vlocal(:,1);
+    QValues(:,:,i) = Qlocal(:,:,1);
 end
 return
 for i = size(rawValues,2):-1:1
